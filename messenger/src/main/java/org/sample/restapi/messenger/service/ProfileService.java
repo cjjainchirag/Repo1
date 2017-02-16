5dd539138e857f55b6f773c1d741fceba13c3e5e
cjain5 <cjain5@sapient.com>
2017-02-16 15:14:56 +0000

    Repo 1

---
+++ messenger/src/main/java/org/sample/restapi/messenger/service/ProfileService.java
@@ -0,0 +1,47 @@
+package org.sample.restapi.messenger.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.sample.restapi.messenger.database.DatabaseClass;
+import org.sample.restapi.messenger.model.Message;
+import org.sample.restapi.messenger.model.Profile;
+
+public class ProfileService {
+
+
+	private Map<String, Profile> profiles = DatabaseClass.getProfiles();
+	
+	public ProfileService() {
+		profiles.put("CJ", new Profile(1L, "CJ", "Chirag", "Jain"));
+	}
+	
+	public List<Profile> getAllProfiles() {
+		return new ArrayList<Profile>(profiles.values());
+	}
+	
+	public Profile getProfile(String profileName){
+		return profiles.get(profileName);
+	}
+	
+	public Profile addProfile(Profile profile) {
+		profile.setId(profiles.size()+1);
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+	
+	public Profile updateProfile(Profile profile) {
+		if(profile.getProfileName().isEmpty()){
+			return null;
+		}
+		 
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+	
+	public Profile removeProfile(String profileName){
+		return profiles.remove(profileName);
+	}
+
+}
